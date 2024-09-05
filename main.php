587657765b0fa9aa2db3a97ab0c8e4142bc0c93e
Cristnunezj <141673137+Cristnunezj@users.noreply.github.com>
2024-09-05 14:11:36 -0600

    Configura conexión a base de datos y ajusta archivo .env

--- main.php
+++ main.php
@@ -3,24 +3,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $latitud = $_POST['latitud'];
     $longitud = $_POST['longitud'];
 
-    // Conexión a la base de datos
-    $servername = "localhost";
-    $username = "root";
-    $dbname = "db_camion";
+    // Configuración de conexión a la base de datos usando variables de entorno
+    $servername = getenv('localhost');
+    $username = getenv('root');
+    $password = getenv('');
+    $dbname = getenv('db_camion');
 
+    // Crear conexión
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    // Verificar conexión
     if ($conn->connect_error) {
         die("Conexión fallida: " . $conn->connect_error);
     }
 
-    $sql = "INSERT INTO localizaciones (latitud, longitud) VALUES ('$latitud', '$longitud')";
+    // Preparar y ejecutar la consulta
+    $stmt = $conn->prepare("INSERT INTO localizaciones (latitud, longitud) VALUES (?, ?)");
+    $stmt->bind_param("dd", $latitud, $longitud);
 
-    if ($conn->query($sql) === TRUE) {
+    if ($stmt->execute()) {
         http_response_code(200);
     } else {
         http_response_code(500);
     }
 
+    // Cerrar conexión
+    $stmt->close();
     $conn->close();
 }
+?>
